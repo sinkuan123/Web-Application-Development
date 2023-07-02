ba6779d0f13f748db57a09cc289eca443b7730f2
Lim Sin Kuan <limsinkuan123gmail.com>
2023-07-02 17:01:04 +0800

    contact us page

--- project/contact_us.php
+++ project/contact_us.php
@@ -14,6 +14,119 @@
         include 'menu.php';
         ?>
 
+        <div class="page-header p-3 pb-1">
+            <h1>Contact Us</h1>
+        </div>
+        <?php
+        if ($_POST) {
+            // include database connection
+            include 'config/database.php';
+            try {
+                // insert query
+                $query = "INSERT INTO contact_us SET firstname=:firstname, lastname=:lastname, email=:email, phone_number=:phone_number, message=:message, expired_date=:expired_date, created=:created";
+                // prepare query for execution
+                $stmt = $con->prepare($query);
+                $firstname = $_POST['firstname'];
+                $lastname = $_POST['lastname'];
+                $email = $_POST['email'];
+                $phone_number = $_POST['phone_number'];
+                //$phone_pattern = "/^[0-9]{3}-[0-9]{7}$/";
+                //$pattern_011 = "/^[0-9]{3}-[0-9]{8}$/";
+                //$f3numbers = substr($phone_number, 0, 3);
+                $message = $_POST['message'];
+
+                $errorMessage = array();
+
+                if (empty($firstname)) {
+                    $errorMessage[] = "First Name field is empty.";
+                }
+                if (empty($lastname)) {
+                    $errorMessage[] = "Last Name field is empty.";
+                }
+                if (empty($email)) {
+                    $errorMessage[] = "Email field is empty.";
+                } else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                    $errorMessage[] = "Please follow the Email format.";
+                }
+                if (empty($phone_number)) {
+                    $errorMessage[] = "Phone Number field is empty.";
+                } else if (!is_numeric($phone_number)) {
+                    $errorMessage[] = "Phone Number must be numbers only.";
+                }
+                /*  if ($f3numbers == "011") {
+                    if (!preg_match($pattern_011, $phone_number)) {
+                        $errorMessage[] = "Please insert a valid Phone Numbers. E.g. 011-12345678";
+                    }
+                } else if (!preg_match($phone_pattern, $phone_number)) {
+                    $errorMessage[] = "Please insert a valid Phone Numbers. E.g. 012-1234567";
+                }*/
+
+                if (empty($message)) {
+                    $errorMessage[] = "message field is empty.";
+                }
+
+                if (!empty($errorMessage)) {
+                    echo "<div class='alert alert-danger m-3'>";
+                    foreach ($errorMessage as $displayErrorMessage) {
+                        echo $displayErrorMessage . "<br>";
+                    }
+                    echo "</div>";
+                } else {
+                    // Bind the parameters
+                    $stmt->bindParam(':firstname', $firstname);
+                    $stmt->bindParam(':lastname', $lastname);
+                    $stmt->bindParam(':email', $email);
+                    $stmt->bindParam(':phone_number', $phone_number);
+                    $stmt->bindParam(':message', $message);
+
+                    // Execute the query
+                    if ($stmt->execute()) {
+                        echo "<div class='alert alert-success m-3'>Record was saved.</div>";
+                        $_POST = array();
+                    } else {
+                        echo "<div class='alert alert-danger m-3'>Unable to save the record.</div>";
+                    }
+                }
+            }
+            // show error
+            catch (PDOException $exception) {
+                die('ERROR: ' . $exception->getMessage());
+            }
+        }
+        ?>
+        <div class="p-3">
+            <!-- html form here where the product information will be entered -->
+            <form action="<?php echo $_SERVER["PHP_SELF"]; ?>" method="POST">
+                <table class='table table-hover table-responsive table-bordered'>
+                    <tr>
+                        <td>First Name</td>
+                        <td><input type='text' name='firstname' id='firstname' class='form-control' value="<?php echo isset($_POST['firstname']) ? $_POST['firstname'] : ''; ?>" /></td>
+                    </tr>
+                    <tr>
+                        <td>Last Name</td>
+                        <td><input type="text" name='lastname' id='lastname' class='form-control' value="<?php echo isset($_POST['lastname']) ? $_POST['lastname'] : ''; ?>" /></td>
+                    </tr>
+                    <tr>
+                        <td>Email</td>
+                        <td><input type='email' name='email' id='email' class='form-control' value="<?php echo isset($_POST['email']) ? $_POST['email'] : ''; ?>" /></td>
+                    </tr>
+                    <tr>
+                        <td>Phone Number</td>
+                        <td><input type="text" name='phone_number' id='phone_number' class='form-control' value="<?php echo isset($_POST['phone_number']) ? $_POST['phone_number'] : ''; ?>" /></td>
+                    </tr>
+                    <tr>
+                        <td>Message</td>
+                        <td><textarea name='message' class='form-control'><?php echo isset($_POST['message']) ? $_POST['message'] : ''; ?></textarea></td>
+                    </tr>
+                    <td></td>
+                    <td>
+                        <input type='submit' value='Save' class='btn btn-primary' />
+                        <a href='index.php' class='btn btn-danger'>Back to read products</a>
+                    </td>
+                    </tr>
+                </table>
+            </form>
+        </div>
     </div>
     <!-- end container -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
--- project/customer_create.php
+++ project/customer_create.php
@@ -17,7 +17,88 @@
         <div class="page-header p-3 pb-1">
             <h1>Create Customer</h1>
         </div>
+        <?php
+        if ($_POST) {
+            // include database connection
+            include 'config/database.php';
+            try {
+                // insert query
+                $query = "INSERT INTO customers SET user_name=:user_name, user_password=:user_password, first_name=:first_name, last_name=:last_name, gender=:gender, date_of_birth=:date_of_birth, registration_date_time=:registration_date_time, account_status=:account_status";
+                // prepare query for execution
+                $stmt = $con->prepare($query);
+                $user_name = $_POST['user_name'];
+                $user_password = $_POST['user_password'];
+                $first_name = $_POST['first_name'];
+                $last_name = $_POST['last_name'];
+                $gender = $_POST['gender'];
+                $date_of_birth = $_POST['date_of_birth'];
+                $account_status = $_POST['account_status'];
+                $usernamePattern = "/^[A-Za-z][A-Za-z0-9_-]{5,}$/";
+                $passwordPattern = "/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{6,}$/";
 
+                $errorMessage = array();
+
+                if (empty($user_name)) {
+                    $errorMessage[] = "User Name field is empty.";
+                } else if (!preg_match($usernamePattern, $user_name)) {
+                    $errorMessage[] = "Username must be at least 6 characters long, start with a letter, and can only contain letters, digits, '_', or '-'.";
+                }
+                if (empty($user_password)) {
+                    $errorMessage[] = "Password field is empty.";
+                } else if (!preg_match($passwordPattern, $user_password)) {
+                    $errorMessage[] = "Password must be at least 6 characters long and contain at least one uppercase letter, one lowercase letter, and one number. No special symbols allowed.";
+                }
+                if (empty($first_name)) {
+                    $errorMessage[] = "First Name field is empty.";
+                }
+                if (empty($last_name)) {
+                    $errorMessage[] = "Last Name field is empty.";
+                }
+                if (empty($gender)) {
+                    $errorMessage[] = "Gender field is empty.";
+                }
+                if (empty($date_of_birth)) {
+                    $errorMessage[] = "Date of Birth field is empty.";
+                }
+                if (empty($account_status)) {
+                    $errorMessage[] = "Account Status field is empty.";
+                }
+
+
+                if (!empty($errorMessage)) {
+                    echo "<div class='alert alert-danger m-3'>";
+                    foreach ($errorMessage as $displayErrorMessage) {
+                        echo $displayErrorMessage . "<br>";
+                    }
+                    echo "</div>";
+                } else {
+                    // Bind the parameters
+                    $stmt->bindParam(':user_name', $user_name);
+                    $stmt->bindParam(':user_password', $user_password);
+                    $stmt->bindParam(':first_name', $first_name);
+                    $stmt->bindParam(':last_name', $last_name);
+                    $stmt->bindParam(':gender', $gender);
+                    $stmt->bindParam(':date_of_birth', $date_of_birth);
+                    date_default_timezone_set('Asia/Kuala_Lumpur');
+                    $registration_date_time = date('Y-m-d H:i:s');
+                    $stmt->bindParam(':registration_date_time', $registration_date_time);
+                    $stmt->bindParam(':account_status', $account_status);
+
+                    // Execute the query
+                    if ($stmt->execute()) {
+                        echo "<div class='alert alert-success m-3'>Record was saved.</div>";
+                        $_POST = array();
+                    } else {
+                        echo "<div class='alert alert-danger m-3'>Unable to save the record.</div>";
+                    }
+                }
+            }
+            // show error
+            catch (PDOException $exception) {
+                die('ERROR: ' . $exception->getMessage());
+            }
+        }
+        ?>
 
 
         <div class="p-3">
@@ -26,11 +107,11 @@
                 <table class='table table-hover table-responsive table-bordered'>
                     <tr>
                         <td>User Name</td>
-                        <td><input type='text' name='user_name' id='user_name' class='form-control' value="<?php echo isset($_POST['user_name']) ? $_POST['user_name'] : ''; ?>" /></td>
+                        <td><input type='text' name='user_name' id='user_name' class='form-control' pattern="[A-Za-z][A-Za-z0-9_-]{5,}" value="<?php echo isset($_POST['user_name']) ? $_POST['user_name'] : ''; ?>" /></td>
                     </tr>
                     <tr>
                         <td>Password</td>
-                        <td><input type="user_password" name='user_password' id='user_password' class='form-control' value="<?php echo isset($_POST['user_password']) ? $_POST['user_password'] : ''; ?>" /></textarea></td>
+                        <td><input type="password" name='user_password' id='user_password' class='form-control' value="<?php echo isset($_POST['user_password']) ? $_POST['user_password'] : ''; ?>" /></td>
                     </tr>
                     <tr>
                         <td>First Name</td>
@@ -55,10 +136,6 @@
                         <td><input type='date' name='date_of_birth' class='form-control' value="<?php echo isset($_POST['date_of_birth']) ? $_POST['date_of_birth'] : ''; ?>" /></td>
                     </tr>
                     <tr>
-                        <td>Registration Date and Time</td>
-                        <td><input type="datetime-local" name='registration_date_time' class='form-control' value="<?php echo isset($_POST['registration_date_time']) ? $_POST['registration_date_time'] : ''; ?>" /></td>
-                    </tr>
-                    <tr>
                         <td>Account Status</td>
                         <td><input type="radio" id="active" name="account_status" value="Active" checked>
                             <label for="active">Active</label><br>
@@ -75,90 +152,7 @@
                     </tr>
                 </table>
             </form>
-        </div><?php
-                if ($_POST) {
-                    // include database connection
-                    include 'config/database.php';
-                    try {
-                        // insert query
-                        $query = "INSERT INTO customers SET user_name=:user_name, user_password=:user_password, first_name=:first_name, last_name=:last_name, gender=:gender, date_of_birth=:date_of_birth, registration_date_time=:registration_date_time, account_status=:account_status";
-                        // prepare query for execution
-                        $stmt = $con->prepare($query);
-                        $user_name = $_POST['user_name'];
-                        $user_password = $_POST['user_password'];
-                        $first_name = $_POST['first_name'];
-                        $last_name = $_POST['last_name'];
-                        $gender = $_POST['gender'];
-                        $date_of_birth = $_POST['date_of_birth'];
-                        $registration_date_time = $_POST['registration_date_time'];
-                        $account_status = $_POST['account_status'];
-                        //$usernamePattern = "/^[A-Za-z][A-Za-z0-9_-]{5,}$/";
-                        //$passwordPattern = "/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{6,}$/";
-
-                        $errorMessage = array();
-
-                        if (empty($user_name)) {
-                            $errorMessage[] = "User Name field is empty.";
-                        } /*else if (!preg_match($usernamePattern, $user_name)) {
-                            $errorMessage[] = "Please follow the User Name format.";
-                        }*/
-                        if (empty($user_password)) {
-                            $errorMessage[] = "Password field is empty.";
-                        } /*else if (preg_match($passwordPattern, $user_password)) {
-                            $errorMessage[] = "Please follow the Password format.";
-                        }*/
-                        if (empty($first_name)) {
-                            $errorMessage[] = "First Name field is empty.";
-                        }
-                        if (empty($last_name)) {
-                            $errorMessage[] = "Last Name field is empty.";
-                        }
-                        if (empty($gender)) {
-                            $errorMessage[] = "Gender field is empty.";
-                        }
-                        if (empty($date_of_birth)) {
-                            $errorMessage[] = "Date of Birth field is empty.";
-                        }
-                        if (empty($registration_date_time)) {
-                            $errorMessage[] = "Registration Date Time field is empty.";
-                        }
-                        if (empty($account_status)) {
-                            $errorMessage[] = "Account Status field is empty.";
-                        }
-
-
-                        if (!empty($errorMessage)) {
-                            echo "<div class='alert alert-danger m-3'>";
-                            foreach ($errorMessage as $displayErrorMessage) {
-                                echo $displayErrorMessage . "<br>";
-                            }
-                            echo "</div>";
-                        } else {
-                            // Bind the parameters
-                            $stmt->bindParam(':user_name', $user_name);
-                            $stmt->bindParam(':user_password', $user_password);
-                            $stmt->bindParam(':first_name', $first_name);
-                            $stmt->bindParam(':last_name', $last_name);
-                            $stmt->bindParam(':gender', $gender);
-                            $stmt->bindParam(':date_of_birth', $date_of_birth);
-                            $stmt->bindParam(':registration_date_time', $registration_date_time);
-                            $stmt->bindParam(':account_status', $account_status);
-
-                            // Execute the query
-                            if ($stmt->execute()) {
-                                echo "<div class='alert alert-success m-3'>Record was saved.</div>";
-                                $_POST = array();
-                            } else {
-                                echo "<div class='alert alert-danger m-3'>Unable to save the record.</div>";
-                            }
-                        }
-                    }
-                    // show error
-                    catch (PDOException $exception) {
-                        die('ERROR: ' . $exception->getMessage());
-                    }
-                }
-                ?>
+        </div>
     </div>
     <!-- end container -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
