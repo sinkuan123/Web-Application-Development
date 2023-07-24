defe3c80643be4f8908f02a0969396033171e46c
Lim Sin Kuan <limsinkuan123gmail.com>
2023-07-24 15:13:35 +0800

    add javascript to add product row

--- project/order_create.php
+++ project/order_create.php
@@ -31,15 +31,35 @@
         if ($_POST) {
             try {
                 $error = array();
-                for ($i = 1; $i <= 3; $i++) {
-                    $quantity = $_POST["quantity{$i}"];
-                    if (empty($quantity)) {
-                        $error[] = "Please fill in the quantity for product " . $i . ".";
-                    } else if ($quantity == 0) {
-                        $error[] = "Quantity Can not be zero.";
-                    }
+
+                if (isset($_POST['customer'])) {
+                    $customer_id = $_POST['customer'];
+                } else {
+                    $error[] = "Please choose your user name.";
                 }
 
+                // var_dump($_POST);
+                if (isset($_POST['product'])) {
+                    $selected_product_count = count($_POST['product']);
+                } else {
+                    $error[] = "Select unless one product.";
+                }
+                if (isset($selected_product_count)) {
+                    for ($i = 0; $i < $selected_product_count; $i++) {
+
+                        $product_id = $_POST["product"];
+                        $quantity = $_POST["quantity"];
+                        if (!isset($product_id)) {
+                            $error[] = " Please choose the product for NO " . $i + 1 . ".";
+                        }
+
+                        if (empty($quantity)) {
+                            $error[] = "Please fill in the quantity for product NO " . $i + 1 . ".";
+                        } else if ($quantity == 0) {
+                            $error[] = "Quantity Can not be zero.";
+                        }
+                    }
+                }
                 if (!empty($error)) {
                     echo "<div class='alert alert-danger role='alert'>";
                     foreach ($error as $error_message) {
@@ -57,19 +77,18 @@
                     $order_sumary_stmt->bindParam(":order_date", $order_date);
                     $order_sumary_stmt->execute();
 
-                    $order_id = $con->lastInsertId(); //Get the order_id that last inserted.
+                    $order_id = $con->lastInsertId(); //Get the order_id from last inserted row.
 
-                    for ($i = 1; $i <= 3; $i++) {
-                        $product_id = $_POST["product{$i}"];
-                        $quantity = $_POST["quantity{$i}"];
+                    for ($i = 0; $i < $selected_product_count; $i++) {
+                        $product_id = $_POST["product"];
+                        $quantity = $_POST["quantity"];
 
                         $order_detail_query = "INSERT INTO order_detail SET order_id=:order_id, customer_id=:customer_id, product_id=:product_id, quantity=:quantity";
                         $order_detail_stmt = $con->prepare($order_detail_query);
                         $order_detail_stmt->bindParam(":order_id", $order_id);
                         $order_detail_stmt->bindParam(":customer_id", $customer_id);
-
-                        $order_detail_stmt->bindParam(":product_id", $product_id);
-                        $order_detail_stmt->bindParam(":quantity", $quantity);
+                        $order_detail_stmt->bindParam(":product_id", $product_id[$i]);
+                        $order_detail_stmt->bindParam(":quantity", $quantity[$i]);
                         $order_detail_stmt->execute();
                     }
                     echo "<div class='alert alert-success' role='alert'>Order Placed Successfully.</div>";
@@ -82,38 +101,90 @@
         <div>
             <form action="" method="post">
                 <select name="customer" id="customer" class="form-select w-50 my-4">
+                    <option value="" selected hidden disabled>Choose your name</option>
                     <?php
                     foreach ($customers as $customer) {
                         echo "<option value='{$customer['customer_id']}'>{$customer['user_name']}</option>";
                     }
                     ?>
                 </select>
-                <table class="table table-hover table-responsive table-bordered">
+                <table class="table table-hover table-responsive table-bordered" id="row_del">
                     <tr>
+                        <th>NO.</th>
                         <th>Product</th>
                         <th>Quantity</th>
+                        <th>Actions</th>
                     </tr>
-                    <?php for ($i = 1; $i <= 3; $i++) : ?>
-                        <tr>
-                            <td>
-                                <select name="product<?php echo $i ?>" id="product" class="form-select">
-                                    <?php
-                                    foreach ($products as $product) {
-                                        echo "<option value='{$product['id']}'>{$product['name']}</option>";
-                                    }
-                                    ?>
-                                </select>
-                            </td>
-                            <td>
-                                <input type="number" class="form-control" name="quantity<?php echo $i ?>" id="quantity" min="1">
-                            </td>
-                        </tr>
-                    <?php endfor ?>
+                    <tr class="pRow">
+                        <td class="col-1">
+                            1
+                        </td>
+                        <td>
+                            <select name="product[]" id="product" class="form-select">
+                                <option value="" selected disabled hidden>Choose a Product</option>
+                                <?php
+                                foreach ($products as $product) {
+                                    echo "<option value='{$product['id']}'>{$product['name']}</option>";
+                                }
+                                ?>
+                            </select>
+                        </td>
+                        <td>
+                            <input type="number" class="form-control" name="quantity[]" id="quantity" min="1">
+                        </td>
+                        <td>
+                            <input href='#' onclick='deleteRow(this)' class='btn d-flex justify-content-center btn-danger mt-1' readonly value="Delete" />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td>
+                            <input type="button" value="Add More Product" class="btn btn-success add_one" />
+                        </td>
+                        <td></td>
+                    </tr>
                 </table>
+
                 <div class="text-center">
                     <button type="submit" class="btn btn-primary">Place Order</button>
+                    <a href="" class="btn btn-danger">Back to Read Order Sumary</a>
                 </div>
             </form>
+            <script>
+                document.addEventListener('click', function(event) {
+                    if (event.target.matches('.add_one')) {
+                        var rows = document.getElementsByClassName('pRow');
+                        // Get the last row in the table
+                        var lastRow = rows[rows.length - 1];
+                        // Clone the last row
+                        var clone = lastRow.cloneNode(true);
+                        // Insert the clone after the last row
+                        lastRow.insertAdjacentElement('afterend', clone);
+
+                        // Loop through the rows
+                        for (var i = 0; i < rows.length; i++) {
+                            // Set the inner HTML of the first cell to the current loop iteration number
+                            rows[i].cells[0].innerHTML = i + 1;
+                        }
+                    }
+                }, false);
+
+                function deleteRow(r) {
+                    var total = document.querySelectorAll('.pRow').length;
+                    if (total > 1) {
+                        var i = r.parentNode.parentNode.rowIndex;
+                        document.getElementById("row_del").deleteRow(i);
+
+                        var rows = document.getElementsByClassName('pRow');
+                        for (var i = 0; i < rows.length; i++) {
+                            // Set the inner HTML of the first cell to the current loop iteration number
+                            rows[i].cells[0].innerHTML = i + 1;
+                        }
+                    } else {
+                        alert("You need order at least one item.");
+                    }
+                }
+            </script>
         </div>
     </div> <!-- end .container -->
 
