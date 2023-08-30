dc850c8b56ae2afde8b20bd0ec11eb087e32f48c
Lim Sin Kuan <limsinkuan123gmail.com>
2023-08-30 23:34:47 +0800

    change title

--- project/order_detail_read.php
+++ project/order_detail_read.php
@@ -3,7 +3,7 @@
 <html>
 
 <head>
-    <title>PDO - Create a Record - PHP CRUD Tutorial</title>
+    <title>Read Order Detail</title>
     <!-- Latest compiled and minified Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 
